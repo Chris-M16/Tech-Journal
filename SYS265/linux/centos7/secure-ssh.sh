a60c8ba08bfe17b789bd0136c5d93f80c9c7ad1b
Chris-M16 <christopher.mchugh@mymail.champlain.edu>
2022-02-16 15:37:14 -0500

    Script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,12 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo #ALL YOUR CODE GOES HERE"
+echo Welcome to passwordless user creation \(you must be sudo/root user to execute properly\)
+echo What would you like the username to be?  
+read user
+sudo useradd -m -d /home/$user -s /bin/bash $user -p '*'
+sudo mkdir /home/$user/.ssh
+sudo cp ~/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+sudo chmod 700 /home/$user/.ssh
+sudo chmod 600 /home/$user/.ssh/authorized_keys
+sudo chown -R $user:$user /home/$user/.ssh
